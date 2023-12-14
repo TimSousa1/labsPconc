22ec95444f5dc8c4d30ac47a6d0dfb02ceef68c9
Peter <peporod12@gmail.com>
2023-12-14 14:01:46 +0000

    I tried with strings but failedc

--- lab5/lab5-pipe-strings.c
+++ lab5/lab5-pipe-strings.c
@@ -5,11 +5,30 @@
 #include <stdlib.h>
 
 #define MAX_SIZE 100
+#define N_THREADS 4
+
+char **pipe_fd[2];
+
+void *thread_string_size(void *arg) {
+	char *string;
+	while(1) {
+		if (read(pipe_fd[0], string, MAX_SIZE) < 1) pthread_exit(NULL);
+		printf("String: %s has %d characters", string, strlen(string));
+	}
+}
 
 int main(int argc, char** argv) {
 
-	char *string[MAX_SIZE];
-	fgets(string, MAX_SIZE, stdio);
+	char string[MAX_SIZE];
+	pthread_t thread_ids[N_THREADS];
 
+	for (int i = 0; i < N_THREADS; i++) {
+		pthread_create(&thread_ids[i], NULL, thread_string_size, NULL);
+	}
+
+	while(fgets(string, MAX_SIZE, stdin)) {
+		write(pipe_fd[1], string, MAX_SIZE);
+
+	}
 }
 
